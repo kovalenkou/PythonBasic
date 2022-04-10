3efcc67a48e59b628acb4e84b14b556f6d8aef5a
yuriikovalenko <anekosheik@gmail.com>
2022-04-10 20:00:12 +0300

    create homework

---
+++ dz2_Calculator_0.1.py
@@ -0,0 +1,8 @@
+#homework 2 - Calculator 0.1
+print('Hello! This is the finest calculator (version 0.1)')
+print('This version only can add two integer')
+num1 = int(input('Please, input the first integer\n'))
+num2 = int(input('Please, input the second integer\n'))
+sum = num1 + num2
+print(num1,  '+', num2, '=', sum)
+print('Done!')
