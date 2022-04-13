960862d79c020dd6e34afab85e002f1dda1aeb89
yuriikovalenko <anekosheik@gmail.com>
2022-04-13 19:11:54 +0300

    Change variables name with Style Guide for Python Code

--- dz1_stresslevel.py
+++ dz1_stresslevel.py
@@ -1,3 +1,3 @@
 #homework 1 - Stress Level meter
-StressLevel = int(input('Hi! This is a Stress Level measurer.\nInput your level in positive digit.\n'))
-print('A' * StressLevel + '!')
+stress_level = int(input('Hi! This is a Stress Level measurer.\nInput your level in positive digit.\n'))
+print('A' * stress_level + '!')
