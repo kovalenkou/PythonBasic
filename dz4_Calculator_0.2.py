a1051e64175d6b04a44e037a1be6deaebfed2303
kovalenkou <anekosheik@gmail.com>
2022-04-18 21:27:34 +0300

    new varsion the code

--- dz4_Calculator_0.2.py
+++ dz4_Calculator_0.2.py
@@ -2,32 +2,27 @@
 print('Hello! This is the finest calculator (version 0.2)')
 print('This version can plus or minus or multiply or divide two integers')
 operation_type = input('Please, choose the type of the operation.\nInput + or - or * or / to continue:\n')
-text_input1 = 'Please, input the first integer\n'
-text_input2 = 'Please, input the second integer\n'
+# check the alloved type of operations
+if  operation_type in ('+','-','*','/'):
+    num1 = int(input('Please, input the first integer\n'))
+    num2 = int(input('Please, input the second integer\n'))
+else:
+    print('Incorect input the type of the operation!')
+# calculate the result
 result = None
 if  operation_type == '+':
-    num1 = int(input(text_input1))
-    num2 = int(input(text_input2))
     result = num1 + num2
     print(num1,  '+', num2, '=', result)
 elif operation_type == '-':
-    num1 = int(input(text_input1))
-    num2 = int(input(text_input2))
     result = num1 - num2
     print(num1,  '-', num2, '=', result)
 elif operation_type == '*':
-    num1 = int(input(text_input1))
-    num2 = int(input(text_input2))
     result = num1 * num2
     print(num1,  '*', num2, '=', result)
 elif operation_type == '/':
-    num1 = int(input(text_input1))
-    num2 = int(input(text_input2))
     if num2 == 0:
         print('Incorect: division by zero')
     else:
         result = num1 / num2
         print(num1,  '/', num2, '=', result)
-else:
-    print('Incorect input')
 print('Done!')
